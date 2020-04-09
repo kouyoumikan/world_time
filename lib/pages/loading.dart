1c2d3daed2adcb9fe1a3e2401184b2f183eee614
小林英里香 <kouyoumikan136@gmail.com>
2020-04-10 08:53:08 +0900

    2020/04/09 #30 Passing Route Data ホームページへリダイレクトする

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,18 +8,14 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = "loading";
-
   // WorldTimeクラスの新しいインスタンスを作成
   void setupWorldTime() async {  // データを取得する同期メソッド
     // WorldTimeインスタンス(場所フラグとURL)
     WorldTime instance = WorldTime(location: "Belin", flag: "germany.png", url: "Europe/Berlin");
     await instance.getTime(); // 内部の時間を設定して取得する
-    print(instance.time); // 取得した時間を console に表示
 
-    setState(() { // 設定状態を使用して、timeプロパティを更新する
-      time = instance.time; // timeプロパティとインスタンス時間を等しく設定
-    });
+    Navigator.pushReplacementNamed(context, "/home");
+
   }
 
   @override
@@ -33,7 +29,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child: Text(time), // app画面に取得した日付/時刻を表示
+        child: Text("loading"), // app画面に取得した日付/時刻を表示
       ),
     );
   }
