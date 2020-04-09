4d74d270d82b7b6c257b53d95d45bd0162693ac1
小林英里香 <kouyoumikan136@gmail.com>
2020-04-09 17:03:20 +0900

    2020/04/09 #29 Error Handling WorldTimeデータ取得の際、エラーが発生時のイベント console ベルリンのデータ(日付、時間)を出力エラー内容を出力

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -13,23 +13,28 @@ class WorldTime {
 
   Future<void> getTime() async { // データを取得する非同期メソッド
 
-    // make the response
-    http.Response response = await http.get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    //print((data));
+    try { // データ取得
+      // make the response
+      http.Response response = await http.get('http://worldtimeapi.org/api/aaa/$url');
+      Map data = jsonDecode(response.body);
+      //print((data));
 
-    // get properties from data
-    String datetime = data["datetime"]; // 日付・時刻
-    String offset = data["utc_offset"].substring(1,3); // 部分文字列の作成
-    //print(datetime);
-    //print(offset);
+      // get properties from data
+      String datetime = data["datetime"]; // 日付・時刻
+      String offset = data["utc_offset"].substring(1,3); // 部分文字列の作成
+      //print(datetime);
+      //print(offset);
 
-    //create DateTime object
-    DateTime now = DateTime.parse(datetime); // 文字列を日付/時刻オブジェクトに変換
-    now = now.add(Duration(hours: int.parse(offset))); // 文字列を整数に変換
+      //create DateTime object
+      DateTime now = DateTime.parse(datetime); // 文字列を日付/時刻オブジェクトに変換
+      now = now.add(Duration(hours: int.parse(offset))); // 文字列を整数に変換
 
-    // set the time property(時間を設定する)
-    time = now.toString();
+      // set the time property(時間を設定する)
+      time = now.toString();
+    }
+    catch (e) { // データ取得の際、エラーが発生した時のイベント
+      print("caught error: $e");
+    }
   }
 
 }
