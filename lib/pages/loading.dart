614f5d299155a00c33a2453383c367b516fab1fd
小林英里香 <kouyoumikan136@gmail.com>
2020-04-09 16:20:49 +0900

    2020/04/09 #28 WorldTime Custom Class WorldTimeの新しいインスタンスを作成 console ベルリンのデータ(日付、時間)を出力する

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:jp/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,30 +7,18 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
-  void getTime() async { // データを取得する非同期メソッド
-
-    // make the response
-    http.Response response = await http.get('http://worldtimeapi.org/api/timezone/Europe/London');
-    Map data = jsonDecode(response.body);
-    //print((data));
-
-    // get properties from data
-    String datetime = data["datetime"]; // 日付・時刻
-    String offset = data["utc_offset"].substring(1,3); // 部分文字列の作成
-    //print(datetime);
-    //print(offset);
-
-    //create DateTime object
-    DateTime now = DateTime.parse(datetime); // 文字列を日付/時刻オブジェクトに変換
-    now = now.add(Duration(hours: int.parse(offset))); // 文字列を整数に変換
-    print(now);
+  // WorldTimeクラスの新しいインスタンスを作成
+  void setupWorldTime() async {  // データを取得する同期メソッド
+    // WorldTimeインスタンス(場所フラグとURL)
+    WorldTime instance = WorldTime(location: "Belin", flag: "germany.png", url: "Europe/Berlin");
+    await instance.getTime(); // 内部の時間を設定して取得する
+    print(instance.time); // 取得した時間を console に表示
   }
 
   @override
   void initState() { // 初期化メソッド(最初に呼び出されるメソッド)
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
---
+++ lib/services/world_time.dart
@@ -0,0 +1,36 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; // location name for the UI(UIの場所名)
+  String time;     // the time in that location(上記の時間)
+  String flag;     // url to an asset flag icon(フラグアイコンへのURL)
+  String url;      // location url for api endpoint(APIエンドポイントのURL)
+
+  // WorldTimeの新しいインスタンスを作成
+  WorldTime({ this.location, this.flag, this.url });
+
+  Future<void> getTime() async { // データを取得する非同期メソッド
+
+    // make the response
+    http.Response response = await http.get('http://worldtimeapi.org/api/timezone/$url');
+    Map data = jsonDecode(response.body);
+    //print((data));
+
+    // get properties from data
+    String datetime = data["datetime"]; // 日付・時刻
+    String offset = data["utc_offset"].substring(1,3); // 部分文字列の作成
+    //print(datetime);
+    //print(offset);
+
+    //create DateTime object
+    DateTime now = DateTime.parse(datetime); // 文字列を日付/時刻オブジェクトに変換
+    now = now.add(Duration(hours: int.parse(offset))); // 文字列を整数に変換
+
+    // set the time property(時間を設定する)
+    time = now.toString();
+  }
+
+}
+
