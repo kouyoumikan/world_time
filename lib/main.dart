4a52a2fd9a4a76937e6892a95a004cee3dd65c7c
小林英里香 <kouyoumikan136@gmail.com>
2020-04-09 09:18:54 +0900

    2020/04/09 #23 Maps & Routing 各ルートを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:jp/pages/home.dart';
+import 'package:jp/pages/choose_location.dart';
+import 'package:jp/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: "/home", // アプリを開いた時にロードする最初のルート
+  routes: {
+    "/": (context) => Loading(), // ロード画面
+    "/home": (context) => Home(), // ホーム画面
+    "/location": (context) => ChooseLocation(), // 位置、場所
+  },
 ));
 
