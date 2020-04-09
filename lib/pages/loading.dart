c367d260ebde1143cf4762f14170e698134fa3b8
小林英里香 <kouyoumikan136@gmail.com>
2020-04-09 16:43:10 +0900

    2020/04/09 #28 WorldTime Custom Class app画面に取得した日付/時間を表示 console ベルリンのデータ(日付、時間)を出力する

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -7,12 +7,19 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  String time = "loading";
+
   // WorldTimeクラスの新しいインスタンスを作成
   void setupWorldTime() async {  // データを取得する同期メソッド
     // WorldTimeインスタンス(場所フラグとURL)
     WorldTime instance = WorldTime(location: "Belin", flag: "germany.png", url: "Europe/Berlin");
     await instance.getTime(); // 内部の時間を設定して取得する
     print(instance.time); // 取得した時間を console に表示
+
+    setState(() { // 設定状態を使用して、timeプロパティを更新する
+      time = instance.time; // timeプロパティとインスタンス時間を等しく設定
+    });
   }
 
   @override
@@ -24,7 +31,10 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("loading screen"),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time), // app画面に取得した日付/時刻を表示
+      ),
     );
   }
 }
