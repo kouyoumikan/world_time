1742f21b1de91d01cba1b7b2a5d124988cdc3619
小林英里香 <kouyoumikan136@gmail.com>
2020-04-10 12:06:52 +0900

    2020/04/10 #33 Ternary Operators 背景画像を設定し、アプリ画面全体に表示

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,40 +15,51 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context).settings.arguments;
     print(data);
 
+    // set background(昼間か夜間の背景画像を設定)
+    String bgImage = data["isDaytime"] ? "day.jpeg" : "night.jpeg";
+
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                  onPressed: () { // アイコンボタン押下時イベント
-                    Navigator.pushNamed(context, "/location");
-                  },
-                  icon: Icon(Icons.edit_location),
-                  label: Text("Edit Location"),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text( // アプリ画面に取得したデータのlocation(場所)を表示
-                    data["location"],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text( // アプリ画面に取得したデータのtime(時間)を表示
-                data["time"],
-                style: TextStyle(
-                  fontSize: 66.0,
+        child: Container(
+          decoration: BoxDecoration( // 背景画像の設定
+        image: DecorationImage( // アプリ画面全体に画像を配置
+              image: AssetImage("assets/night.jpeg"),
+              fit: BoxFit.cover, // アプリ画面全体をカバーする
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                    onPressed: () { // アイコンボタン押下時イベント
+                      Navigator.pushNamed(context, "/location");
+                    },
+                    icon: Icon(Icons.edit_location),
+                    label: Text("Edit Location"),
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text( // アプリ画面に取得したデータのlocation(場所)を表示
+                      data["location"],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text( // アプリ画面に取得したデータのtime(時間)を表示
+                  data["time"],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
